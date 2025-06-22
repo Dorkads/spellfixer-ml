36e427fdccb0e9b2328aa39f5b6ba4b3fe662c4b
Ruslan <homastavi@gmail.com>
2025-06-23 00:20:06 +0300

    Создание скрипта для деплоя основных файлов модели на сервер

--- scripts/scr.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd, json
-df = pd.read_csv("data/processed/dataset.csv")
-corr = dict(zip(df["noisy"], df["correct"]))
-json.dump(corr, open("../spellfixer-backend/corrections.json","w"), ensure_ascii=False, indent=2)
